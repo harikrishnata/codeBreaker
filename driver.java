7b14dc9b9a052552f34a60d8b3dbfd6126ac32ff
harikrishnata <h.thekketharaamarjeevan@lancaster.ac.uk>
2022-03-13 21:48:58 +0000

    Program now checks user input and gives feedback

--- driver.java
+++ driver.java
@@ -2,7 +2,7 @@ public class driver{
     public static int currentGuess=0;
     public static int currentElement=0;
 
-
+    public static int[] code;
 
     public static void main(String[] args){
         TitleScreen G = new TitleScreen();
