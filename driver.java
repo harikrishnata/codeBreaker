1578cdd75b6ba559440b3f47e47dd5485dc148db
harikrishnata <h.thekketharaamarjeevan@lancaster.ac.uk>
2022-03-19 23:28:10 +0000

    Added encapsulation to some public variables and made them private

--- driver.java
+++ driver.java
@@ -1,19 +1,56 @@
 public class driver 
 {
-    
+
     public static GameGUI G;
-
-    public static int codeLength;
-    public static int numOfGuesses;
-
-    public static int[] code;
-
-    public static int currentGuess = 0;
-    public static int currentElement = 0;
     public static boolean done=false;
     public static boolean won=false;
+
+    private static int codeLength;
+    private static int numOfGuesses;
+    private static int[] code;
+    private static int currentGuess = 0;
+    private static int currentElement = 0;
     public static void main(String[] args) 
     {
         TitleScreen T = new TitleScreen();
     }
+
+
+    public static void setCodeLength(int x){
+        codeLength=x;
+    }
+    public static int getCodeLength(){
+        return codeLength;
+    }
+
+    public static void setNumOfGuesses(int x){
+        numOfGuesses=x;
+    }
+    public static int getNumOfGuesses(){
+        return numOfGuesses;
+    }   
+    
+    public static void setCurrentGuess(int x){
+        currentGuess=x;
+    }
+    public static int getCurrentGuess(){
+        return currentGuess;
+    }  
+
+    public static void setCurrentElement(int x){
+        currentElement=x;
+    }
+    public static int getCurrentElement(){
+        return currentElement;
+    }
+
+    public static void setCode(int[] x){
+        code=x;
+    }
+    public static void setCodeElement(int position, int x){
+        code[position]=x;
+    }
+    public static int[] getCode(){
+        return code;
+    }    
 }
